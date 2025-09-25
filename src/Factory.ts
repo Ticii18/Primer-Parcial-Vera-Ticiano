5e56d17bead85c415c84faa3e12cf10d45e292f7
Ticii18 <ticianovera22@gmail.com>
2025-09-25 12:03:15 -0300

    first commit

---
+++ src/Factory.ts
@@ -0,0 +1,51 @@
+
+interface Equipo {
+  tipo: string;
+  nombre: string;
+  RAM: string;
+  procesador: string;
+  detalles(): string;
+}
+
+class Notebook implements Equipo {
+  tipo = "Notebook";
+  constructor(public nombre: string, public RAM: string, public procesador: string) {}
+  detalles() {
+    return `Tipo: ${this.tipo}, Nombre: ${this.nombre}, RAM: ${this.RAM}, Procesador: ${this.procesador}`;
+  }
+}
+
+class Desktop implements Equipo {
+  tipo = "Desktop";
+  constructor(public nombre: string, public RAM: string, public procesador: string) {}
+  detalles() {
+    return `Tipo: ${this.tipo}, Nombre: ${this.nombre}, RAM: ${this.RAM}, Procesador: ${this.procesador}`;
+  }
+}
+
+class Servidor implements Equipo {
+  tipo = "Servidor";
+  constructor(public nombre: string, public RAM: string, public procesador: string) {}
+  detalles() {
+    return `Tipo: ${this.tipo}, Nombre: ${this.nombre}, RAM: ${this.RAM}, Procesador: ${this.procesador}`;
+  }
+}
+
+class EquipoFactory {
+  crearEquipo(tipo: string, nombre:string, ram:string, procesador:string) {
+    switch (tipo) {
+      case "Notebook":
+        return new Notebook(nombre, ram , procesador);
+      case "Desktop":
+        return new Desktop(nombre, ram , procesador);
+      case "Servidor":
+        return new Servidor(nombre, ram , procesador);
+      default:
+        throw new Error("tipo de equipo no reconocido");
+    }
+  }
+}
+
+const factory = new EquipoFactory()
+const server = factory.crearEquipo("Servidor", "Dell PowerEdge","32GB", "Xeon")
+console.log(server.detalles())
---
+++ src/app.ts
@@ -0,0 +1,29 @@
+class InventarioViejo {
+  private items: { nombre: string }[] = [];
+  addItem(nombre: string) {
+    this.items.push({ nombre });
+  }
+  getItems() {
+    return this.items;
+  }
+}
+
+class AdaptadorInventario {
+  constructor(private inventarioViejo: InventarioViejo) {}
+
+  addItem(item: { nombre: string; tipo: string; estado: string }) {
+    this.inventarioViejo.addItem(item.nombre);
+  }
+
+  getItems() {
+    return this.inventarioViejo.getItems();
+  }
+}
+
+
+const inventarioViejo = new InventarioViejo();
+const adaptador = new AdaptadorInventario(inventarioViejo);
+
+adaptador.addItem({ nombre: "Router Cisco", tipo: "Red", estado: "disponible" });
+console.log(adaptador.getItems());
+
